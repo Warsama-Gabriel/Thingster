3d03961c98ba5f6fe2716324d2bb9c16072f9de7
chrome-tempest <srobleh@gmail.com>
2014-06-05 10:55:51 -0700

    added user validations

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,10 +1,11 @@
 class Thing < ActiveRecord::Base
 
   belongs_to :user
+
+  validates :user_id, presence:true
   
   validates :name, presence: true,
                    length: { minimum: 3, maximum: 250 }
-
   validates :description, length: { maximum: 1000 }
 
 end
