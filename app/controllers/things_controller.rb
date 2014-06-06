e0cfd48c6181a45f976081eb2298e4549b7215cb
chrome-tempest <srobleh@gmail.com>
2014-06-05 17:02:09 -0700

    added things authorization and mythings index page

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,46 +1,36 @@
 class ThingsController < ApplicationController
-  before_action :set_thing, only: [:show, :edit, :update, :destroy]
+  before_action :set_thing, only: [:show, :edit, :update, :destroy] #this happens first, because check user depends on it
+  before_action :check_user, only: [:edit, :destroy]
 
   before_action :authenticate_user!
 
-  # GET /things
-  # GET /things.json
   def index
     @things =Thing.all
   end
 
-  # GET /things/1
-  # GET /things/1.json
+  def new
+    @thing = current_user.things.new
+  end
+
   def show
   end
-
-  # GET /things/new
-  def new
-    @thing = Thing.new
-  end
-
-  # GET /things/1/edit
+  
   def edit
   end
 
-  # POST /things
-  # POST /things.json
   def create
-    @thing = Thing.new(thing_params)
-
-    respond_to do |format|
-      if @thing.save
-        format.html { redirect_to @thing, notice: 'Thing was successfully created.' }
-        format.json { render :show, status: :created, location: @thing }
-      else
-        format.html { render :new }
-        format.json { render json: @thing.errors, status: :unprocessable_entity }
+    @thing = current_user.things.new(thing_params)
+      respond_to do |format|
+        if @thing.save
+          format.html { redirect_to @thing, notice: 'Thing was successfully created.' }
+          format.json { render :show, status: :created, location: @thing }
+        else
+          format.html { render :new }
+          format.json { render json: @thing.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
 
-  # PATCH/PUT /things/1
-  # PATCH/PUT /things/1.json
   def update
     respond_to do |format|
       if @thing.update(thing_params)
@@ -52,9 +42,7 @@ class ThingsController < ApplicationController
       end
     end
   end
-
-  # DELETE /things/1
-  # DELETE /things/1.json
+  
   def destroy
     @thing.destroy
     respond_to do |format|
@@ -63,13 +51,24 @@ class ThingsController < ApplicationController
     end
   end
 
+  def mythings
+    @things = current_user.things.all
+  end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_thing
       @thing = Thing.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def check_user
+      if current_user.id == @thing.user_id
+      else
+        flash[:alert] = ("You don't have permission to do that")
+        redirect_to action: :index
+      end
+    end
+
     def thing_params
       params.require(:thing).permit(:name, :description)
     end
