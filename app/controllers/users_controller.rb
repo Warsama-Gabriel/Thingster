07957677f682031771dc0f69fc213e51db7eea44
chrome-tempest <srobleh@gmail.com>
2014-06-09 10:16:15 -0700

    added user views, updated readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    # if user_signed_in?
-    #   redirect_to :controller => 'pages', :action => 'helloworld'
   end
 
 end
